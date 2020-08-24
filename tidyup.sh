a67f8b28185243b280756d491eb3ca1a82895181
Terence Broadbent <44728186+BroadbentT@users.noreply.github.com>
2020-08-24 19:17:26 +0100

    Add files via upload

---
+++ tidyup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# coding:UTF-8
+
+# -------------------------------------------------------------------------------------
+#                      Windows Powershell Reverse Shell Script
+#                BY TERENCE BROADBENT BSC CYBER SECURITY (FIRST CLASS)
+# -------------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------------------
+# AUTHOR  : Terence Broadbent                                                    
+# CONTRACT: GitHub                                                               
+# Version : 2.0                                                                
+# Details : Tidyup files.
+# Modified: N/A                                                               
+# -------------------------------------------------------------------------------------
+
+rm file.txt
+rm output.txt
+
+
